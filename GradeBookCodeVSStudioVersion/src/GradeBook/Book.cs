67538f91f37e422460aa3ff505f4436953c8e6e5
lordb90 <lordb90@gamil.com>
2019-09-24 15:48:03 -0400

    VStudioEdtion not Core

---
+++ GradeBookCodeVSStudioVersion/src/GradeBook/Book.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBookCodeVSStudioVersion
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            Grades = new List<double>();
+            Name = name;
+        }
+
+        public List<double> Grades { get; set; }
+        public string Name { get; set; }
+
+        public void AddGrade(double grade)
+        {
+            Grades.Add(grade);
+        }
+
+        public void ShowStatistics()
+        {
+            var average = GetCalculatedAverage();
+            var lowGrade = GetMinGrade();
+            var highGrade = GetMaxGrade();
+            Console.WriteLine($"Grades:");
+            Console.WriteLine($"Lowest: {lowGrade}");
+            Console.WriteLine($"Average: {average}");
+            Console.WriteLine($"Highest: {highGrade}");
+        }
+
+        private double GetCalculatedAverage()
+        {
+            double total = 0;
+            foreach (var grade in Grades)
+            {
+                total += (double)grade;
+            }
+
+            return total / Grades.Count;
+        }
+
+        private double GetMaxGrade()
+        {
+            var maxGrade = double.MinValue;
+
+            foreach (var grade in Grades)
+            {
+                maxGrade = Math.Max(maxGrade, grade);
+            }
+            return maxGrade;
+        }
+
+        private double GetMinGrade()
+        {
+            var maxGrade = double.MaxValue;
+
+            foreach (var grade in Grades)
+            {
+                maxGrade = Math.Min(maxGrade, grade);
+            }
+            return maxGrade;
+        }
+    }
+}
---
+++ GradeBookCodeVSStudioVersion/src/GradeBook/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GradeBookCodeVSStudioVersion
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var book = new Book("Test Grades");
+
+            //book.AddGrade(89.0);
+            book.AddGrade(92.0);
+            book.AddGrade(90.0);
+            book.ShowStatistics();
+        }
+    }
+}
